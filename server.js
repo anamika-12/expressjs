10fca54e393e46b1657280a3742e3528c410801f
anamika patel <anamika.patel@globant.com>
2018-05-22 16:47:41 +0530

    added name

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Anamika patel\n This is just for test.');
+  res.send('Hello Anamika patel :)\n This is just for test.');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`); 
+console.log(`Running on http:${HOST}:${PORT}`);
