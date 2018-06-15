742426d2dca99977289f55fcfe24a3b7fcdedab3
anamika patel <anamika.patel@globant.com>
2018-06-15 15:25:21 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Anamika patel :)');
+  res.send('Hello Anamika patel test:)');
 });
 
 app.listen(PORT, HOST);
